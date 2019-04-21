6cb94d489ade8d044abb0591f22ff70b6cc002a7
fmehdaou <49277798+fmehdaou@users.noreply.github.com>
2019-04-21 21:07:09 +0100

    src

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Nav from './components/Nav.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Nav /> 
+      </div>
+    );
+  }
+}
+export default App;
---
+++ Nav.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+class Nav extends Component {
+  render() {
+    return (
+      <p>hello world</p>
+    );
+  }
+}
+export default Nav;
